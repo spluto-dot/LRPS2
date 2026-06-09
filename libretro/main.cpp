e3b0111841192851a65a86d7342aeb08eb4f5967
spluto-dot <brunoraji@ymail.com>
2026-06-09 01:03:32 -0300

    Remove unused pad settings configuration code
    Removed unused input settings code for pad configuration.

--- libretro/main.cpp
+++ libretro/main.cpp
@@ -48,7 +48,6 @@
 #include "../pcsx2/Patch.h"
 
 #include "../pcsx2/SPU2/spu2.h"
-#include "../pcsx2/PAD/PAD.h"
 
 #ifdef HAVE_PARALLEL_GS
 extern std::unique_ptr<GSRendererPGS> g_pgs_renderer;
@@ -1186,81 +1185,6 @@ static void check_variables(bool first_run)
 				"EECycleSkip", setting_ee_cycle_skip);
 			updated = true;
 		}
-	}
-
-	char input_settings[32];
-	for (int i = 0; i < 2; ++i)
-	{
-		var.key = input_settings;
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_axis_scale%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-			pad_settings[i].axis_scale = atof(var.value) / 100;
-
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_axis_deadzone%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-			pad_settings[i].axis_deadzone = atoi(var.value) * 32767 / 100;
-
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_button_deadzone%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-			pad_settings[i].button_deadzone = atoi(var.value) * 32767 / 100;
-
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_enable_rumble%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-			pad_settings[i].rumble_scale = atof(var.value) / 100;
-
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_invert_left_stick%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-		{
-			if (!strcmp(var.value, "disabled"))
-			{
-				pad_settings[i].axis_invert_lx = 1;
-				pad_settings[i].axis_invert_ly = 1;
-			}
-			else if (!strcmp(var.value, "x_axis"))
-			{
-				pad_settings[i].axis_invert_lx = -1;
-				pad_settings[i].axis_invert_ly = 1;
-			}
-			else if (!strcmp(var.value, "y_axis"))
-			{
-				pad_settings[i].axis_invert_lx = 1;
-				pad_settings[i].axis_invert_ly = -1;
-			}
-			else if (!strcmp(var.value, "all"))
-			{
-				pad_settings[i].axis_invert_lx = -1;
-				pad_settings[i].axis_invert_ly = -1;
-			}
-		}
-
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_invert_right_stick%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-		{
-			if (!strcmp(var.value, "disabled"))
-			{
-				pad_settings[i].axis_invert_rx = 1;
-				pad_settings[i].axis_invert_ry = 1;
-			}
-			else if (!strcmp(var.value, "x_axis"))
-			{
-				pad_settings[i].axis_invert_rx = -1;
-				pad_settings[i].axis_invert_ry = 1;
-			}
-			else if (!strcmp(var.value, "y_axis"))
-			{
-				pad_settings[i].axis_invert_rx = 1;
-				pad_settings[i].axis_invert_ry = -1;
-			}
-			else if (!strcmp(var.value, "all"))
-			{
-				pad_settings[i].axis_invert_rx = -1;
-				pad_settings[i].axis_invert_ry = -1;
-			}
-		}
-
-		snprintf(input_settings, sizeof(input_settings), "pcsx2_analog_mode%d", i + 1);
-		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
-			pad_settings[i].force_analog = !strcmp(var.value, "enabled");
 	}
 
 	update_option_visibility();
